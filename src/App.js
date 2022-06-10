d3b0f54e63198f6b86fcc93141304cc08db0a601
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-10 07:58:28 -0700

    shekoofeh-shojai-classA

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
  import Header from './components/header';
 import Card from "./components/card/card";
-import CardContiner from "./components/card-continer"
+import CardContainer from "./components/card-continer";
 function App() {
   return (
-    <div >
-     <Header/>
-     <Card/>
-     <CardContiner/>
+    <div>
+      <Header />
+      <Card />
+      <CardContainer />
     </div>
   );
 }
